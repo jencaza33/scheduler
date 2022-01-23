3b8861e27cb26cc455ba8205bff456cd8c8681f8
Jen Caza <jencaza@gmail.com>
2022-01-23 14:08:19 -0500

    update back function and put back interviewer.id in edit mode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,6 +2,24 @@ import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  return { mode };
+  // -- advance to any other mode, replacing current mode in history with a new one
+  const transition = (newMode, replace = false) => {
+    if (!replace) {
+      setHistory((prev) => [...prev, newMode]);
+    }
+    setMode(newMode);
+  }
+
+  // -- transition back to previous mode
+  const back = () => {
+    if (history.length > 1) {
+      const newHistory = [...history];
+      newHistory.pop();
+      setHistory(newHistory);
+      setMode(newHistory[newHistory.length - 1]);
+    }
+  };
+  return { mode, transition, back };
 }
